81cb3ba9eb44c84953f81cfd8a88854801c7bba0
Paulo F. Andrade <paulo@outercorner.com>
2020-10-13 10:03:52 +0100

    Moved to XCFrameworks and SPM binary targets

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CSodium",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v9), .watchOS(.v4)
+        ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,8 +16,10 @@ let package = Package(
         )
     ],
     targets: [
-        .target(
-            name: "CSodium"
+        .binaryTarget(
+            name: "CSodium",
+            url: "https://github.com/OuterCorner/CSodium/releases/download/1.0.0/CSodium.xcframework.zip",
+            checksum: "ce968cae3cf727bdf0c41b2bee884502abeea03bc7a0598b50a14c37ff75bd29"
         )
     ]
 )
